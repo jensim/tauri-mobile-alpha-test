ce9fc27c1885f1ae19155674b34ad0473e7309b4
Jens Brimfors <jens.brimfors@qapital.com>
2022-12-23 13:46:46 +0100

    prep for tauri mobile

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { internalIpV4 } from 'internal-ip'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(async () => {
+  const host = await internalIpV4();
+  return {
+    plugins: [react()],
+    // prevent vite from obscuring rust errors
+    clearScreen: false,
+    server: {
+      host: '0.0.0.0', // listen on all addresses
+      port: 5173,
+      // Tauri expects a fixed port, fail if that port is not available
+      strictPort: true,
+      hmr: {
+        protocol: 'ws',
+        host,
+        port: 5183
+      },
+    },
+    // to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`,
+    // `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG`
+    // env variables
+    envPrefix: ['VITE_', 'TAURI_'],
+    build: {
+      // Tauri supports es2021
+      target: ['es2021', 'chrome100', 'safari13'],
+      // don't minify for debug builds
+      minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+      // produce sourcemaps for debug builds
+      sourcemap: !!process.env.TAURI_DEBUG,
+    }
+  }
 })
